ee44c3af00a3bf95324ea86400d7846e41148151
Sundus Yousuf <sundus.ahmed.yousuf@gmail.com>
2015-02-02 23:12:48 -0600

    Added Diff Tool

--- background.js
+++ background.js
@@ -1,1 +1,19 @@
 console.log('inside background.js');
+chrome.tabs.onUpdated.addListener(function(tabId,changeInfo,tab){
+    if(changeInfo.status=='complete'){
+        if(tab.url.match(/\/pull-requests\/.*\/commits/g)){
+            console.log('Pull Request Page Found');
+            chrome.pageAction.show(tabId);
+        }
+        else {
+            console.log('Pull Request Not Page Found');
+            chrome.pageAction.hide(tabId);
+        }
+    }
+});
+
+chrome.pageAction.onClicked.addListener(function(tab){
+    console.log('icon clicked');
+	chrome.tabs.executeScript({code: 'addDiffTool()'});
+//	chrome.tabs.executeScript({code: 'createLoadingPage()'});
+});
--- js/app/content.js
+++ js/app/content.js
@@ -1,1 +1,14 @@
 console.log('inside content.js');
+
+function addDiffTool(){
+    console.log("Reading Commits");
+    clean();
+    var commits = jQuery("td.changeset:not(:has(span.merge-lozenge))");
+    commits.append("<span class='diff'>DIFF</span>");
+
+}
+
+function clean(){
+    var diff = jQuery("span.diff")
+    diff.remove();
+}
