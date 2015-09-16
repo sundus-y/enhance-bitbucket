b3b7db9af902c38f7bbbf2771b938b1dda3c7007
Sundus Yousuf <sundus.ahmed.yousuf@gmail.com>
2015-09-16 08:29:17 -0500

    New Feature IN REVIEW BUTTON
    * Added a new feature that lets user mark a pull request as in review.
    * Added a new action called addPullRequestTool to handle this feature.
    * This action works only on two pages.
    * One is on the pull request page, if you press the alt key you will see
      a new button called IN REVIEW which if you press create a comment
      called IN REVIEW. The action is maped to the showInReviewButton().
    * The second one is on the list of pull requests page. If you press alt
      in this page, it will higlight pull requests that are IN REVIEW. and
      this action is mapped to the highlight().

--- js/app/content.js
+++ js/app/content.js
@@ -13,9 +13,51 @@ function addDiffTool(){
     });
 }
 
+function addPullRequestTool(){
+    var pull_requests_page = jQuery("h2:contains('Pull requests')");
+    var pull_request_page = jQuery("div.pull-request-content>h3:contains('Activity')");
+    if (pull_requests_page.size() == 1) {highlight();}
+    if (pull_request_page.size() == 1) {showInReviewButton();}
+}
+
+function highlight() {
+    var pull_requests = jQuery('table.pull-requests-table').find('td.id');
+    var project_path = window.location.pathname.match(/\/projects\/(.*)\/pull-requests/)[1];
+    jQuery.each(pull_requests, function (i, pull_request) {
+        var pull_request_num = jQuery(pull_request).find('a')[0].innerHTML.match(/#(.*)/)[1];
+        var activity_url = "https://git.its.uiowa.edu/rest/api/latest/projects/" + project_path +
+            "/pull-requests/" + pull_request_num + "/activities?start=0&limit=100";
+        $.ajax({
+            context: pull_request,
+            url: activity_url
+        }).done(function (data) {
+            var in_review = false;
+            jQuery.each(data.values, function(i,activity){
+               if (activity.action == "COMMENTED" && activity.comment.text == "IN REVIEW") {
+                   in_review = true;
+               }
+            });
+            if (in_review){
+                jQuery(this).closest('tr').css('background-color','rgba(230, 255, 98, 0.87)')
+            }
+        });
+    });
+}
+
+function showInReviewButton(){
+    var details_title = jQuery("div.pull-request-content>div>div>h3:contains('Details')");
+    jQuery(details_title).append(" <span class='in_review'> In Review </span>");
+    jQuery('span.in_review').click(function(){
+        jQuery('form.new-comment-form').find('textarea').val('IN REVIEW');
+        jQuery('form.new-comment-form').find("button:contains('Comment')").click();
+    });
+}
+
 function clean(){
     var diff = jQuery("span.diff");
+    var in_review= jQuery("span.in_review");
     diff.remove();
+    in_review.remove();
 }
 
 function cleanDiff(){
@@ -52,7 +94,10 @@ function renderDiff(diff_page){
 
 jQuery(document).ready(function() {
     document.body.addEventListener('keydown', function(e){
-        if(e.keyCode === 18 /* Alt (option) key */){ addDiffTool(); }
+        if(e.keyCode === 18 /* Alt (option) key */){
+            addDiffTool();
+            addPullRequestTool();
+        }
         return true;
     }, false);
     document.body.addEventListener('keyup', function(e){
